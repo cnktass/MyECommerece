c09e0252a8aacb2fa421920a65b526953f002bfa
Cenk Aktas <cnktass@gmail.com>
2024-10-30 21:57:09 +0300

    ProductControluDIGerceklestirmeVeCreateIslemininGerceklesmesi

--- MyECommerece/Controllers/ProductController.cs
+++ MyECommerece/Controllers/ProductController.cs
@@ -1,12 +1,65 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyECommerece.Data;
+using MyECommerece.Models;
 
 namespace MyECommerece.Controllers
 {
     public class ProductController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        private readonly IWebHostEnvironment _environment;
+
+        public ProductController(ApplicationDbContext context, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _environment = environment;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.ToListAsync();
+            return View(products);
+        }
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Product product, IFormFile Photo)
+        {
+            ModelState.Remove("PhotoPath");
+
+            if (ModelState.IsValid)
+            {
+                if (Photo != null && Photo.Length > 0)
+                {
+                    var uploadsFolder = Path.Combine(_environment.WebRootPath, "uploads");
+                    if (!Directory.Exists(uploadsFolder))
+                    {
+                        Directory.CreateDirectory(uploadsFolder);
+                    }
+
+                    var uniqueFileName = Guid.NewGuid().ToString() + "_" + Path.GetFileName(Photo.FileName);
+                    var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await Photo.CopyToAsync(fileStream);
+                    }
+
+                    product.PhotoPath = "/uploads/" + uniqueFileName;
+                }
+
+                _context.Add(product);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
     }
 }
