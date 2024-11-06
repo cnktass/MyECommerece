87f81fdaf65b3c24ae4c52d3ab072f0e64b9f43b
Cenk Aktas <cnktass@gmail.com>
2024-11-06 21:40:03 +0300

    SepetteMevcutIletisimBilgileriniGostermeVeGuncelleyebilme

--- MyECommerece/Controllers/CartController.cs
+++ MyECommerece/Controllers/CartController.cs
@@ -4,16 +4,19 @@ using MyECommerece.Models;
 using MyECommerece.Data;
 using System.Linq;
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 
 namespace MyECommerece.Controllers
 {
     public class CartController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public CartController(ApplicationDbContext context)
+        public CartController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
         public IActionResult AddToCart(int id)
         {
@@ -40,10 +43,31 @@ namespace MyECommerece.Controllers
             HttpContext.Session.SetObjectAsJson("Cart", cart);
             return RedirectToAction("Index", "Product"); 
         }
-        public IActionResult ViewCart()
+        public async Task<IActionResult> ViewCart()
         {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return Redirect("/Identity/Account/Login");
+            }
             var cart = HttpContext.Session.GetObjectFromJson<List<CartItem>>("Cart") ?? new List<CartItem>();
+            ViewBag.PhoneNumber = user.PhoneNumber;
+            ViewBag.Address = user.Address;
             return View(cart);
+
+        }
+        public async Task<IActionResult> UpdateUserInfo(string phoneNumber, string address)
+        {
+            var user = await _userManager.GetUserAsync (User);
+            if (user == null) 
+            {
+                return Redirect("/Identity/Account/Login");
+            }
+            user.PhoneNumber = phoneNumber;
+            user.Address = address;
+            await _userManager.UpdateAsync(user);
+
+            return RedirectToAction("ViewCart");
         }
     }
 }
